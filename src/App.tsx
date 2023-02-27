780c8229940e01edc2ada5af6d63858166a53a29
Mark Johnson <marklj@gmail.com>
2023-02-26 16:57:18 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
