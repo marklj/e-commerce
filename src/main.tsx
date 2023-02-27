c0d4037f0282c47386b57175dab1f0429a480417
Mark Johnson <marklj@gmail.com>
2023-02-27 08:38:10 -0800

    add basic routing and error handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import ErrorPage from "./error-page";
+import Root from "./root";
 import "./styles.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "/", element: <App /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
